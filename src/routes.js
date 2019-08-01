ed0b93a821328e0fbdd94ca8b71f80ea2f3b4df0
sr9872@naver.com <sr9872@naver.com>
2019-08-01 12:04:15 +0900

    first commit

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import User from './components/users/User.vue';
+import Home from './components/Home.vue'
+
+export const routes = [
+  { path: '', component: Home },
+  { paht: '/user', component: User}
+];
